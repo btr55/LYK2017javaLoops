b6505c95d78b7d38b436164e32a4c4018c662571
Fatih Erdal <fatiherdal0655@gmail.com>
2017-07-25 19:29:09 +0300

    ilk

---
+++ src/tr/org/linux/kamp/whileCounter/WhileCounter.java
@@ -0,0 +1,11 @@
+package tr.org.linux.kamp.whileCounter;
+
+public class WhileCounter {
+public static void main(String[] args) {
+	int counter = 0;
+	while(counter<=10) {
+		System.out.println(counter);
+		++counter;
+	}
+}
+}
